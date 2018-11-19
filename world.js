c24238f9d5accecd29ed7954a1b2125715cd6f40
Morgan K <morgan.kennedy@mymona.uwi.edu>
2018-11-19 07:05:01 +0000

    Exercise 4 - All countries

--- world.js
+++ world.js
@@ -1,19 +1,28 @@
 window.onload = function(){
     var search = document.getElementById('lookup');
-    console.log(search)
+    var lookup = document.getElementById('country');
     search.addEventListener('click',function(){
-        console.log(2)
+
         var request = new XMLHttpRequest();
         request.onreadystatechange = searchCountry;
-        console.log(document.getElementById('country').value)
-        request.open('GET',"world.php?country=" + document.getElementById('country').value);
-        request.send();
+        if(document.getElementById('checkbox').checked==true){
+            request.open('GET',"world.php?all=true",true);
+            request.send();
+        }
+        else{
+            if(document.getElementById('country').value!=""){
+            request.open('GET',"world.php?country=" + document.getElementById('country').value,true);
+            request.send();
+            }
+        
+            else{
+                document.getElementById('result').innerHTML = "";
+            }
+        } 
         function searchCountry(){
-            console.log(request);
             if (request.readyState === XMLHttpRequest.DONE) {
                 if (request.status === 200) {
                     document.getElementById('result').innerHTML = request.responseText;
-                    console.log(22)
                 } else {
                     alert('There was a problem with the request.' );
                 }
--- world.php
+++ world.php
@@ -5,12 +5,18 @@ $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 $country = $_GET['country'];
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
+$getAll = $_GET['all'];
+$stmt2 = $conn ->query("SELECT * FROM countries");
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if($getAll == 'true')
+    $results = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+else{
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);  
+}
+
+
 
 echo '<ul>';
 foreach ($results as $row) {
